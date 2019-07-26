e1823448cf7f47c24a78b830cfc83ba5396b0177
pengbaolin <39398162+pengbaolin@users.noreply.github.com>
2019-07-27 00:40:12 +0800

    Adapt to new parameter reading style;

--- convlab/human_eval/bot_server.py
+++ convlab/human_eval/bot_server.py
@@ -23,16 +23,24 @@ app = Flask(__name__)
 os.environ['lab_mode'] = 'eval'
 spec_file = sys.argv[1]
 spec_name = sys.argv[2]
-lab_mode, prename = sys.argv[3].split('@')
-spec = spec_util.get_eval_spec(spec_file, prename)
+lab_mode = sys.argv[3]
+
+if '@' in lab_mode:
+    lab_mode, prename = lab_mode.split('@')
+    spec = spec_util.get_eval_spec(spec_file, spec_name, prename)
+else:
+    spec = spec_util.get(spec_file, spec_name)
+
+# # lab_mode, prename = sys.argv[3].split('@')
+# spec = spec_util.get_eval_spec(spec_file, prename)
 spec = spec_util.override_eval_spec(spec)
 agent_spec = spec['agent'][0]
 env = make_env(spec)
 body = Body(env, spec['agent'])
 agent = DialogAgent(spec, body)
 
-last_obs = 'hi'
-agent.reset(last_obs)
+# last_obs = 'hi'
+# agent.reset(last_obs)
 
 
 # obs = 'hi can you find me a hotel in the west?'
@@ -62,18 +70,19 @@ def process():
 def generate_response(in_queue, out_queue):
     while True:
         # pop input
+        last_action = 'null'
         in_request = in_queue.get()
         obs = in_request['input']
 
         if in_request['agent_state'] == {}:
             agent.reset(obs)
         else:
-            encoded_state, dst_state = in_request['agent_state']
+            encoded_state, dst_state, last_action = in_request['agent_state']
             agent.body.encoded_state = np.asarray(encoded_state) if isinstance(encoded_state, list) else encoded_state
             agent.dst.state = copy.deepcopy(dst_state)
+            agent.update(obs, last_action, 0, obs, 0)
         try:
             action = agent.act(obs)
-            agent.update(obs, action, 0, obs, 0)
             encoded_state = agent.body.encoded_state.tolist() if isinstance(agent.body.encoded_state,
                                                                             np.ndarray) else agent.body.encoded_state
             dst_state = copy.deepcopy(agent.dst.state)
@@ -82,14 +91,15 @@ def generate_response(in_queue, out_queue):
 
         try:
             if action == '':
-                response = 'Sorry I donot understand, can you paraphrase?'
+                response = 'Sorry I do not understand, can you paraphrase?'
             else:
                 response = action
         except Exception as e:
             print('Response generation error', e)
             response = 'What did you say?'
 
-        out_queue.put({'response': response, 'agent_state': (encoded_state, dst_state)})
+        last_action = action
+        out_queue.put({'response': response, 'agent_state': (encoded_state, dst_state, last_action)})
         in_queue.task_done()
         out_queue.join()
 
