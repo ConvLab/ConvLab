bcb920cae5bb87e1b8890bbc55520846b8d889e8
truthless11 <truthless11@gmail.com>
2019-09-11 16:04:01 +0800

    add HDSA policy

---
+++ convlab/modules/word_policy/multiwoz/hdsa/hdsa.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 11 14:58:00 2019
+
+@author: truthless
+"""
+
+from convlab.modules.word_policy.multiwoz.hdsa.predictor import HDSA_predictor
+from convlab.modules.word_policy.multiwoz.hdsa.generator import HDSA_generator
+from convlab.modules.policy.system.policy import SysPolicy
+
+class HDSA(SysPolicy):
+    
+    def __init__(self, archive_file, model_file=None, use_cuda=False):
+        self.predictor = HDSA_predictor(archive_file, model_file, use_cuda)
+        self.generator = HDSA_generator(archive_file, model_file, use_cuda)
+        
+    def init_session(self):
+        self.generator.init_session()
+        
+    def predict(self, state):
+        
+        act, kb = self.predictor.predict(state)
+        response = self.generator.generate(state, act, kb)
+        
+        return response
