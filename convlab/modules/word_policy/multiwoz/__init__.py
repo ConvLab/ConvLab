83c435ab44cbca8c96c2aacfdabf94cfedf7a9fb
Jinchao Li <jincli@microsoft.com>
2019-09-20 18:05:04 -0700

    add larl model

--- convlab/modules/word_policy/multiwoz/__init__.py
+++ convlab/modules/word_policy/multiwoz/__init__.py
@@ -2,3 +2,4 @@
 # Licensed under the MIT license.
 
 from convlab.modules.word_policy.multiwoz.mdrg.policy import MDRGWordPolicy
+from convlab.modules.word_policy.multiwoz.larl.policy import LaRLPolicy
